8625f69fefcb551685792d725305c9e9c36a89b1
incihuseynli <inci.huseynli0@gmail.com>
2023-08-23 11:01:33 +0400

    fix issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+// Responsive Mobile Menu
+
 const openedMenu = document.querySelector('#openedMenu');
 const closedMenu = document.querySelector('#closedMenu');
-const smNavMenu = document.querySelector('.sm.nav-menu');
+const smNavMenu = document.querySelector('.navbar-sm');
 
 openedMenu.addEventListener('click', ()=> {
     smNavMenu.style.transform = "translateY(0%)";
@@ -8,3 +10,22 @@ openedMenu.addEventListener('click', ()=> {
 closedMenu.addEventListener('click', ()=> {
     smNavMenu.style.transform = "translateY(-200%)";
 });
+
+
+// Page Up Button
+const toTop = document.getElementById("toTop");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+}
+function pageUp() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
