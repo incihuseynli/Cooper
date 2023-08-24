2642ce3251c4898a5d5b10e2888fc0b23b14469c
incihuseynli <inci.huseynli0@gmail.com>
2023-08-24 16:59:05 +0400

    fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 // Responsive Mobile Menu
 
-const openedMenu = document.querySelector('#openedMenu');
-const closedMenu = document.querySelector('#closedMenu');
-const smNavMenu = document.querySelector('.navbar-sm');
+const openedMenu = document.querySelector("#openedMenu");
+const closedMenu = document.querySelector("#closedMenu");
+const smNavMenu = document.querySelector(".navbar-sm");
 
-openedMenu.addEventListener('click', ()=> {
-    smNavMenu.style.transform = "translateY(0%)";
+openedMenu.addEventListener("click", () => {
+  smNavMenu.style.transform = "translateY(0%)";
 });
-closedMenu.addEventListener('click', ()=> {
-    smNavMenu.style.transform = "translateY(-200%)";
+closedMenu.addEventListener("click", () => {
+  smNavMenu.style.transform = "translateY(-200%)";
 });
-
 
 // Page Up Button
 const toTop = document.getElementById("toTop");
 
-
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -29,3 +29,30 @@ function pageUp() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Searching Products in Catalog
+
+const searchProducts = () => {
+  const searchProduct = document
+    .querySelector("#searchInput")
+    .value.toUpperCase();
+  const products = document.querySelector(".products-list");
+  const product = document.querySelectorAll(".card");
+  const productName = products.getElementsByTagName("h3");
+
+  for (let i = 0; i < productName.length; i++) {
+    let checkMatch = product[i].getElementsByTagName("h3")[0];
+
+    if (checkMatch) {
+      let condition = checkMatch.textContent || checkMatch.innerHTML;
+      if (condition.toUpperCase().indexOf(searchProduct) > -1) {
+        product[i].style.display = "";
+        // console.log(product[i]);
+      } else {
+        product[i].style.display = "none";
+      }
+    }
+  }
+};
+
+// Products Filter by Product Price
